c0166afb30884629d37d6fb0efa85023f35f92c8
Márk Mados <mark.mados@rapidimages.se>
2024-06-12 13:10:28 +0200

    Added image to first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Sunburned Memoirs</h1>
+        <img alt="sunset logo" src="favicon.ico" width={"100px"} />
       </header>
     </div>
   );
