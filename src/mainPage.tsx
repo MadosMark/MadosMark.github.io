ad1d01d9f2368c18fd68b66909d02998077c7567
Márk Mados <mark.mados@rapidimages.se>
2024-08-12 16:31:08 +0200

    added playsInline

--- src/mainPage.tsx
+++ src/mainPage.tsx
@@ -121,6 +121,7 @@ function MainPage() {
                   autoPlay={true}
                   loop={true}
                   muted
+                  playsInline
                 >
                   <source src={item.src} type="video/mp4" />
                   Your browser does not support the video tag.
