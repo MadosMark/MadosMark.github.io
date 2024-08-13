3b5885077f4ad698c00f8cd7c473250e788d4b8d
Márk Mados <mark.mados@rapidimages.se>
2024-08-13 11:42:24 +0200

    Removed animation

--- src/mainPage.tsx
+++ src/mainPage.tsx
@@ -232,7 +232,7 @@ function MainPage() {
           </div>
         </div>
         <div ref={portfolioRef} className="portfolioSection scrollSection">
-          {/* {portfolio.map((item, index) => {
+          {portfolio.map((item, index) => {
             if (item.type === "image") {
               return (
                 <img
@@ -260,46 +260,7 @@ function MainPage() {
             } else {
               return null;
             }
-          })} */}
-          <div className="gallery">
-            <input type="checkbox" />
-            <img
-              src="https://picsum.photos/id/1028/300/300"
-              alt="a forest after an apocalypse"
-            />
-            <img
-              src="https://picsum.photos/id/15/300/300"
-              alt="a waterfall and many rocks"
-            />
-            <img
-              src="https://picsum.photos/id/1040/300/300"
-              alt="a house on a mountain"
-            />
-            <img
-              src="https://picsum.photos/id/106/300/300"
-              alt="sime pink flowers"
-            />
-            <img
-              src="https://picsum.photos/id/136/300/300"
-              alt="big rocks with some trees"
-            />
-            <img
-              src="https://picsum.photos/id/1039/300/300"
-              alt="a waterfall, a lot of tree and a great view from the sky"
-            />
-            <img
-              src="https://picsum.photos/id/110/300/300"
-              alt="a cool landscape"
-            />
-            <img
-              src="https://picsum.photos/id/1047/300/300"
-              alt="inside a town between two big buildings"
-            />
-            <img
-              src="https://picsum.photos/id/1057/300/300"
-              alt="a great view of the sea above the mountain"
-            />
-          </div>
+          })}
         </div>
         <div ref={contactRef} className="contactSection scrollSection">
           <h2>Contact Us</h2>
