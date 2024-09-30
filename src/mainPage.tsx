a441af7036a5fb48ba70b7bc77c7ddbcbc6b1ea6
Márk Mados <mark.mados@rapidimages.se>
2024-09-30 11:02:47 +0200

    removed scroll icon

--- src/mainPage.tsx
+++ src/mainPage.tsx
@@ -205,7 +205,6 @@ function MainPage() {
                 transition={{ duration: 1.5 }}
                 className="textTwo"
               ></motion.div>
-              <div className="mouse"></div>
             </div>
           </div>
           <div ref={aboutRef} className="aboutSection scrollSection">
@@ -234,16 +233,6 @@ function MainPage() {
                     preserveAspectRatio="xMidYMid slice"
                     viewBox="10 10 80 80"
                   >
-                    {/* <path
-                      fill="#000"
-                      className="out-top"
-                      d="M37-5C25.1-14.7,5.7-19.1-9.2-10-28.5,1.8-32.7,31.1-19.8,49c15.5,21.5,52.6,22,67.2,2.3C59.4,35,53.7,8.5,37-5Z"
-                    />
-                    <path
-                      fill="#fff"
-                      className="in-top"
-                      d="M20.6,4.1C11.6,1.5-1.9,2.5-8,11.2-16.3,23.1-8.2,45.6,7.4,50S42.1,38.9,41,24.5C40.2,14.1,29.4,6.6,20.6,4.1Z"
-                    /> */}
                     <path
                       fill="#000"
                       className="out-bottom"
